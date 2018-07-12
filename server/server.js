ee56b58f70b53bd0dd1d1da0a01e262daf414343
Your Name <you@example.com>
2018-07-12 11:23:09 +0100

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,17 +14,33 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.on('createMessage', (message) => {
-        console.log('createMessage', message);
+    //socket.emit from Admin text welcome
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
     });
 
-    socket.on('disconnect', () => {
-        console.log('User was disconnected');
+    //socket.broadcast.emit
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
         io.emit('newMessage', {
-            from: message.form,
+            from: message.from,
             text: message.text,
-            createAt: new Date().getTime()
+            createAt: newDate().getTime()
         }); 
+        //socket.broadcast.emit('newMessage', {
+            //from: message.from,
+          //  text: message.text,
+        //    createAt: newDate().getTime()
+      //  });
     });
 });
 
